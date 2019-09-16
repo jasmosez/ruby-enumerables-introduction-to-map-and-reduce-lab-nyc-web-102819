679fa073611b9269c0309a6da07a093f1d31a1d7
James Schaffer <jas.schaffer@gmail.com>
2019-09-16 19:53:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -34,13 +34,8 @@ end
 
 
 def reduce_to_total(source_array, starting_point)
-  i = starting_point
+  if starting_point ? i = starting_point : i = 0
   while i < source_array.length do
-    result += source_array[i]
-    i += 1
-  end
-  i = 0
-  while i < starting_point do
     result += source_array[i]
     i += 1
   end
