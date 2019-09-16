5f01ab6dbf83ce366325f184f96bdaff6bfd2ec9
James Schaffer <jas.schaffer@gmail.com>
2019-09-16 19:44:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,26 @@
-map_to_negativize(source_array)
-map_to_no_change(source_array)
-map_to_double(source_array)
-map_to_square(source_array)
+def map_to_negativize(source_array)
+  i = 0
+  while i < source_array.length do
+    source_array[i] = -source_array[i]
+    i += 1
+  end
+  
+end
 
+def map_to_no_change(source_array)
+end
+
+def map_to_double(source_array)
+end
+
+def map_to_square(source_array)
+end
+
+def reduce_to_total(source_array, starting_point)
+end
+
+def reduce_to_all_true(source_array)
+end
+
+def reduce_to_any_true(source_array)
+end
