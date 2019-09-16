d5635e778c85eed4c7c923f166c99139e62bfb14
James Schaffer <jas.schaffer@gmail.com>
2019-09-16 20:04:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -56,4 +56,12 @@ def reduce_to_all_true(source_array)
 end
 
 def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+    if source_array[i]
+      return TRUE
+    end
+    i += 1
+  end
+  return FALSE
 end
